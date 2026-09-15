ca4f48a3c01697da9b6f7b0749ee4f36abc698c6
ashrafidoost <reza.ashrafidoost@gmail.com>
2026-09-15 12:09:37 -0300

    Padre ginos first app - Ch01

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
+const Pizza = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("ul", {}, props.name),
+    React.createElement(
+      "li",
+      {},
+      React.createElement(
+        "ul",
+        {},
+        React.createElement("li", {}, props.description),
+      ),
+    ),
+  ]);
+};
+
 const App = () => {
   return React.createElement(
     "div",
     {},
     React.createElement("h1", {}, "Padre Gino's"),
+    React.createElement(Pizza, {
+      name: "Pepperoni Pizza",
+      description: "Classic Pizza with pep",
+    }),
+    React.createElement(Pizza, {
+      name: "Margaritta Pizza",
+      description: "Margariiiiiiiiiiiiiiiiiiita",
+    }),
+    React.createElement(Pizza, {
+      name: "Persian Pizza",
+      description: "Pizza that modified for persian taste",
+    }),
+    React.createElement(Pizza, {
+      name: "Pizza Shab",
+      description: "A specific thick pizza.",
+    }),
   );
-  console.log("in the component");
 };
 
 const container = document.getElementById("root");
