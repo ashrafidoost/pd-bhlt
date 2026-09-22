9210c2ba36e76af1694d6dda0e76d483b048f282
ashrafidoost <reza.ashrafidoost@gmail.com>
2026-09-21 23:10:14 -0300

    Padre ginos first app - Ch01

--- src/Pizza.jsx
+++ src/Pizza.jsx
@@ -7,7 +7,7 @@ const Pizza = (props) => {
         <img
           src={props.image}
           alt={props.name}
-          style={{ width: 100, borderRadius: 20 }}
+          style={{ width: 150, borderRadius: 20 }}
         />
       </ul>
     </div>
