6a8cc55fb6289d2e036bc83eac367655ad11e2c4
ashrafidoost <reza.ashrafidoost@gmail.com>
2026-09-23 20:13:50 -0300

    Padre ginos app - Form submission-Cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
-import { StrictMode } from "react";
+import { StrictMode, useState } from "react";
 import { createRoot } from "react-dom/client";
 import Order from "./order";
 import PizzaOfTheDay from "./PizzaOfTheDay";
+import Header from "./Header";
+import { CartContext } from "./contexts";
 
 const App = () => {
-  console.log(">>>>> App component rendered <<<<<");
+  const cartHook = useState([]);
   return (
     <StrictMode>
-      <div>
-        <h1 className="logo">Padre Gino's - Order Now!</h1>
-        <Order />
-        <PizzaOfTheDay />
-      </div>
+      <CartContext.Provider value={cartHook}>
+        <div>
+          <Header />
+          <Order />
+          <PizzaOfTheDay />
+        </div>
+      </CartContext.Provider>
     </StrictMode>
   );
 };
